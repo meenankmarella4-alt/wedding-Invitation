0a1f1de3aebbbbf0783e5e241996025b9e2445a1
meenankmarella4-alt <meenankmarella4@gmail.com>
2026-07-07 00:13:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const openBtn = document.getElementById("openBtn");
 
+const cover = document.getElementById("cover");
+
+const invitation = document.getElementById("invitation");
+
+openBtn.addEventListener("click", function(){
+
+    cover.style.display = "none";
+
+    invitation.style.display = "block";
+
+});
